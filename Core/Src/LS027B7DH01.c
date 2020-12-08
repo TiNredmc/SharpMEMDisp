94604f545859637304da83fbf1f51c41978cd0bd
TiNredmc <thipok17@gmail.com>
2020-12-08 17:19:28 +0700

    See info for more
    - Added better description comments of Display Commands
    - now using malloc to allocate the buffer Heap
    - Update LCD_Update offset equation
    - Experimenting with RBIT (Required for Display data)
    - Update LCD_LoadPart offset equations
    
    Signed-off-by: TiNredmc <thipok17@gmail.com>

--- Core/Src/LS027B7DH01.c
+++ Core/Src/LS027B7DH01.c
@@ -8,21 +8,22 @@
 #include "LS027B7DH01.h"
 #include "font8x8_basic.h"
 #include <string.h>
-
+#include <stdlib.h>
 
 //Display Commands
-uint8_t clearCMD[2] = {0x04,0x00}; // Display Clear
-uint8_t printCMD[2] = {0x01,0x00}; // Display Bitmap (after issued display update)
+uint8_t clearCMD[2] = {0x04,0x00}; // Display Clear 0x04 (HW_LSB)
+uint8_t printCMD[2] = {0x01,0x00}; // Display Bitmap (after issued display update) 0x01 (HW_LSB)
 
 
 //This buffer holds 50 Bytes * 240 Row = 12K of Display buffer
-static uint8_t DispBuf[12000];// entire display buffer.
+uint8_t *DispBuf;// entire display buffer.
 
 //This buffer holds temporary 2 Command bytes
 static uint8_t SendBuf[2];
 
 //This buffer holds 1 Character bitmap image (8x8)
 static uint8_t chBuf[8];
+
 
 // Display Initialization
 void LCD_Init(LS027B7DH01 *MemDisp, SPI_HandleTypeDef *Bus,
@@ -37,13 +38,13 @@ void LCD_Init(LS027B7DH01 *MemDisp, SPI_HandleTypeDef *Bus,
 	MemDisp->LCDcs = LCDcs;
 	MemDisp->LCDon = LCDon;
 
+	DispBuf = malloc(12000);
+
 	HAL_GPIO_WritePin(MemDisp->dispGPIO,MemDisp->LCDon,GPIO_PIN_SET);// Turn display back on
 	//Start 20Hz PWM for COM inversion of the display
 	HAL_TIM_PWM_Start(MemDisp->TimerX,MemDisp->COMpwm);
 	MemDisp->TimerX->Instance->CCR1 = 5;
 
-	//Clean the Buffer
-	memset(DispBuf, 0xFF, 12000);
 
 	//At lease 3 + 13 clock is needed for Display clear (16 Clock = 8x2 bit = 2 byte)
 	HAL_GPIO_WritePin(MemDisp->dispGPIO,MemDisp->LCDcs,GPIO_PIN_SET);
@@ -61,7 +62,7 @@ void LCD_Update(LS027B7DH01 *MemDisp){
 	for(uint8_t count;count < 241;count++){
 	SendBuf[1] = count+1;// counting from row number 1 to row number 240
 	//row to DispBuf offset
-	uint16_t offset = ((count * 50) - 50 <= 0) ? 0 : (count * 50) - 50;
+	uint16_t offset = count * 50;
 
 	HAL_SPI_Transmit(MemDisp->Bus, SendBuf, 2, 150);
 	HAL_SPI_Transmit(MemDisp->Bus, DispBuf+offset, 50, 150);
@@ -89,6 +90,9 @@ void LCD_Clean(LS027B7DH01 *MemDisp){
 
 // Buffer update (full 400*240 pixels)
 void LCD_LoadFull(uint8_t * BMP[]){
+	/*for(uint16_t l; l < 12000; l++){
+		DispBuf[l] = (uint8_t)(__RBIT(BMP[l]) >> 24);
+	}*/
 	memcpy(DispBuf, BMP, 12000);
 }
 
@@ -97,22 +101,22 @@ void LCD_LoadFull(uint8_t * BMP[]){
 //NOTE THAT THE X COOR and WIDTH ARE BYTE NUMBER NOT PIXEL NUMBER (8 pixel = 1 byte). A.K.A IT'S BYTE ALIGNED
 //
 void LCD_LoadPart(uint8_t* BMP[], uint8_t Xcord, uint8_t Ycord, uint8_t bmpW, uint8_t bmpH){
-	if ((bmpW > 50) | (Xcord >50) | (Ycord > 240) | (bmpH > 240)) return;
+	if ((bmpW > 50) | (Xcord > 50) | (Ycord > 240) | (bmpH > 240)) return;
 
 	Xcord = Xcord - 1;
 	Ycord = Ycord - 1;
-	bmpW = bmpW - 1;
-	bmpH = bmpH - 1;
+
 
 	//Counting from Y origin point to bmpH using for loop
 	for(uint8_t loop = 0; loop < bmpH; loop++){
 		// turn X an Y into absolute offset number for Buffer
-		uint16_t XYoff = ( (Ycord + loop == 0) ? 0 : (( (Ycord+loop) * 50 ) - 50) )   +    Xcord;
+		uint16_t XYoff =  (Ycord + loop == 0) ? 0 : ((Ycord+loop) * 50 );
+		XYoff += Xcord;// offset start at the left most, The count from left to right for Xcord times
 
 		// turn W and H into absolute offset number for Bitmap image
-		uint16_t WHoff = ( (loop + 1)*bmpW )- bmpW;
+		uint16_t WHoff = ((loop + 1)*bmpW)- bmpW;
 
-		memcpy(DispBuf + XYoff, BMP+WHoff, bmpW);
+		memcpy(DispBuf + XYoff, BMP + WHoff, bmpW);
 	}
 
 }
