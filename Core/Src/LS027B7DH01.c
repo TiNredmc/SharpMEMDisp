9da17857ffef05c90633dffa11a2a6dd4d358a28
TiNredmc <thipok17@gmail.com>
2020-12-08 21:58:43 +0700

    See info for more - make sure DispBuf init at 0xFF - No need to force (uint8_t *)0x00 - Changed from uint8_t* BMP[] to uint8_t* BMP - pre declare offset value keepers - Fix Inverter (1st row bug) - Added lenght param to LCD_Print

--- Core/Src/LS027B7DH01.c
+++ Core/Src/LS027B7DH01.c
@@ -39,6 +39,7 @@ void LCD_Init(LS027B7DH01 *MemDisp, SPI_HandleTypeDef *Bus,
 	MemDisp->LCDon = LCDon;
 
 	DispBuf = malloc(12000);
+	memset(DispBuf, 0xFF, 12000);
 
 	HAL_GPIO_WritePin(MemDisp->dispGPIO,MemDisp->LCDon,GPIO_PIN_SET);// Turn display back on
 	//Start 20Hz PWM for COM inversion of the display
@@ -68,7 +69,7 @@ void LCD_Update(LS027B7DH01 *MemDisp){
 	HAL_SPI_Transmit(MemDisp->Bus, DispBuf+offset, 50, 150);
 	}
 	//Send the Dummies bytes after whole display data transmission
-	HAL_SPI_Transmit(MemDisp->Bus,(uint8_t *)0x00,2,100);
+	HAL_SPI_Transmit(MemDisp->Bus, 0x00,2,100);
 
 	HAL_GPIO_WritePin(MemDisp->dispGPIO,MemDisp->LCDcs,GPIO_PIN_RESET);// Done
 }
@@ -89,7 +90,7 @@ void LCD_Clean(LS027B7DH01 *MemDisp){
 }
 
 // Buffer update (full 400*240 pixels)
-void LCD_LoadFull(uint8_t * BMP[]){
+void LCD_LoadFull(uint8_t * BMP){
 	/*for(uint16_t l; l < 12000; l++){
 		DispBuf[l] = (uint8_t)(__RBIT(BMP[l]) >> 24);
 	}*/
@@ -100,21 +101,21 @@ void LCD_LoadFull(uint8_t * BMP[]){
 //
 //NOTE THAT THE X COOR and WIDTH ARE BYTE NUMBER NOT PIXEL NUMBER (8 pixel = 1 byte). A.K.A IT'S BYTE ALIGNED
 //
-void LCD_LoadPart(uint8_t* BMP[], uint8_t Xcord, uint8_t Ycord, uint8_t bmpW, uint8_t bmpH){
+void LCD_LoadPart(uint8_t* BMP, uint8_t Xcord, uint8_t Ycord, uint8_t bmpW, uint8_t bmpH){
 	if ((bmpW > 50) | (Xcord > 50) | (Ycord > 240) | (bmpH > 240)) return;
 
 	Xcord = Xcord - 1;
 	Ycord = Ycord - 1;
-
+	uint16_t XYoff,WHoff = 0;
 
 	//Counting from Y origin point to bmpH using for loop
 	for(uint8_t loop = 0; loop < bmpH; loop++){
 		// turn X an Y into absolute offset number for Buffer
-		uint16_t XYoff =  (Ycord + loop == 0) ? 0 : ((Ycord+loop) * 50 );
+		XYoff = (Ycord+loop) * 50;
 		XYoff += Xcord;// offset start at the left most, The count from left to right for Xcord times
 
 		// turn W and H into absolute offset number for Bitmap image
-		uint16_t WHoff = ((loop + 1)*bmpW)- bmpW;
+		WHoff = ((loop + 1)*bmpW)- bmpW;
 
 		memcpy(DispBuf + XYoff, BMP + WHoff, bmpW);
 	}
@@ -123,7 +124,7 @@ void LCD_LoadPart(uint8_t* BMP[], uint8_t Xcord, uint8_t Ycord, uint8_t bmpW, ui
 
 /* FIXME TODO */
 //Similar to LCD_LoadPart, but x,y coordinate are BOTH PIXEL position.
-void LCD_LoadPix(uint8_t* BMP[], uint16_t Xcord, uint8_t Ycord, uint16_t bmpW, uint8_t bmpH){
+void LCD_LoadPix(uint8_t* BMP, uint16_t Xcord, uint8_t Ycord, uint16_t bmpW, uint8_t bmpH){
 	if ((bmpW > 400) | (Xcord >400) | (Ycord > 240) | (bmpH > 240)) return;
 
 	Xcord = Xcord - 1;
@@ -145,8 +146,8 @@ void LCD_LoadPix(uint8_t* BMP[], uint16_t Xcord, uint8_t Ycord, uint16_t bmpW, u
 		uint16_t WHoff = ( (loop + 1)*bmpW )- bmpW;
 
 		for (uint16_t i=0;i < bmpW;i ++){
-			DispBuf[i+XYoff] = (uint8_t)(*BMP[WHoff + i] >> Shiftval);
-			DispBuf[i+XYoff+1] = (uint8_t)(*BMP[WHoff + i] << (7 - Shiftval));
+			DispBuf[i+XYoff] = (uint8_t)(BMP[WHoff + i] >> Shiftval);
+			DispBuf[i+XYoff+1] = (uint8_t)(BMP[WHoff + i] << (7 - Shiftval));
 		}
 
 	}
@@ -157,8 +158,8 @@ void LCD_LoadPix(uint8_t* BMP[], uint16_t Xcord, uint8_t Ycord, uint16_t bmpW, u
 void LCD_Invert(void){
 	uint16_t invt = 12000;
 	do{
-	DispBuf[invt-1] = ~DispBuf[invt-1];
 	invt--;
+	DispBuf[invt] = ~DispBuf[invt];
 	}while(invt);
 }
 
@@ -169,12 +170,12 @@ void LCD_Fill(bool fill){
 
 
 //Print 8x8 Text on screen, TODO Shorten the code
-void LCD_Print(char txtBuf[]){
+void LCD_Print(char txtBuf[], size_t len){
 
-uint16_t strLen = sizeof(*txtBuf);
+uint16_t strLen = len;
 uint8_t YLine = 1;
 uint8_t Xcol = 1;
-uint8_t chOff = 0;
+uint16_t chOff = 0;
 
 for (uint16_t p = 0; p < strLen;p++){
 	// In case of reached 50 chars or newline detected , Do the newline
@@ -187,13 +188,13 @@ for (uint16_t p = 0; p < strLen;p++){
 	// Avoid printing Newline
 	if (*txtBuf != 0x0A){
 
-	chOff = (*txtBuf - 0x20) ? 0 : ( (*txtBuf - 0x20) * 8) - 8 ;// calculate char offset (fist 8 pixel of character)
+	chOff = (*txtBuf - 0x20) * 8;// calculate char offset (fist 8 pixel of character)
 
 	for(uint8_t i=0;i < 8;i++){// Copy the inverted color px to buffer
 	chBuf[i] = ~font8x8_basic[i + chOff];
 	}
 
-	LCD_LoadPart((uint8_t **)chBuf, Xcol, YLine, 1, 8);// Align the char with the 8n pixels
+	LCD_LoadPart((uint8_t *)chBuf, Xcol, YLine, 1, 8);// Align the char with the 8n pixels
 
 	txtBuf++;// move to next char
 	Xcol++;// move cursor to next column
